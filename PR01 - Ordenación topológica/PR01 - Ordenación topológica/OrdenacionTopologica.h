476265d6d443182ed118d11e3d18e9441b8f863a
EduardoSimon <al341513@uji.es>
2016-11-25 14:28:15 +0100

    Añadido Grafos

---
+++ PR01 - Ordenación topológica/PR01 - Ordenación topológica/OrdenacionTopologica.h
@@ -0,0 +1,35 @@
+
+#ifndef _GRAFO_DIRIGIDO_H
+#define _GRAFO_DIRIGIDO__H
+
+#include <vector>
+
+using namespace std;
+
+class GrafoDirigido {
+
+	struct Arco {
+		int origen;
+		int destino;
+		float peso;
+		Arco(int, int, float);
+	};
+
+	struct Vertice {
+		vector<Arco> incidentes; // arcos que entran al vertice
+		vector<Arco> adyacentes; // arcos que salen del vertice
+	};
+
+	vector<Vertice> vertices; // El vertice i estara en la posicion i numerando desde 1
+
+public:
+
+	GrafoDirigido(const char *) throw(string);
+
+	void mostrar() const;
+
+	void mostrarOrdenTopologico() const throw(string);
+
+};
+
+#endif
---
+++ PR01 - Ordenación topológica/PR01 - Ordenación topológica/OrdenaciónTopologica.cpp
@@ -0,0 +1,110 @@
+// g++ GrafoDirigido.cpp -std=c++11 && ./a.out 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <queue>
+
+using namespace std;
+
+
+#include "OrdenacionTopologica.h"
+
+GrafoDirigido::Arco::Arco(int elOrigen, int elDestino, float elPeso)
+	: origen{ elOrigen }, destino{ elDestino }, peso{ elPeso } {
+
+}
+
+void GrafoDirigido::mostrar() const {
+
+	cout << "Listas de adyacencia: " << endl;
+	for (int i = 1; i < vertices.size(); i++) {
+		cout << "  vertice " << i << ":" << endl;
+		for (const auto & arco : vertices[i].adyacentes)
+			cout << "    " << arco.origen << "-->" << arco.destino << " (peso = " << arco.peso << ")" << endl;
+		cout << endl;
+	}
+
+	cout << "Listas de incidencia: " << endl;
+	for (int i = 1; i < vertices.size(); i++) {
+		cout << "  vertice " << i << ":" << endl;
+		for (const auto & arco : vertices[i].incidentes)
+			cout << "    " << arco.origen << "-->" << arco.destino << " (peso = " << arco.peso << ")" << endl;
+		cout << endl;
+	}
+
+}
+
+GrafoDirigido::GrafoDirigido(const char * nombreFichero) throw(string) {
+
+	ifstream flujoEntrada(nombreFichero);
+
+	if (!flujoEntrada.is_open())
+		throw string("ERROR: Imposible abrir el fichero ") + nombreFichero;
+
+	char tipoLinea;
+	int cantidadVertices = -1, origen, destino;
+	float peso;
+
+	while (!flujoEntrada.eof()) {
+		flujoEntrada >> tipoLinea;
+		if (!flujoEntrada.eof()) {
+			if (tipoLinea == 'n') {
+				flujoEntrada >> cantidadVertices;
+				vertices.resize(cantidadVertices + 1); // Numeramos desde 1 dejando la posicion 0 sin usar
+			}
+			else if (tipoLinea == 'a') {
+				if (cantidadVertices == -1) {
+					flujoEntrada.close();
+					throw string("ERROR: falta linea n antes de linea a en ") + nombreFichero;
+				}
+				flujoEntrada >> origen >> destino >> peso;
+				vertices[origen].adyacentes.push_back(Arco(origen, destino, peso));
+				vertices[destino].incidentes.push_back(Arco(origen, destino, peso));
+			}
+		}
+	}
+
+	flujoEntrada.close();
+
+}
+
+void GrafoDirigido::mostrarOrdenTopologico() const throw(string) {
+
+	// A completar en el primer ejercicio
+	vector<int> numeroIncidentes(vertices.size());
+	queue<Vertice> colaVertices;
+
+	for (int i = 1; i < numeroIncidentes.size(); i++) { //Contadores de incidentes
+		numeroIncidentes[i] = vertices[i].incidentes.size();
+	}
+
+	int indiceRaiz = 1;
+
+	while (indiceRaiz < numeroIncidentes.size()) {
+		if (numeroIncidentes[indiceRaiz] == 0) {
+			colaVertices.push(vertices[indiceRaiz]);
+
+		}
+	}
+
+	if (indiceRaiz == numeroIncidentes.size())
+		throw string("El grafo no es acíclico");
+
+
+}
+
+
+int main() {
+
+	try {
+		GrafoDirigido miGrafo("grafoEjemplo2.gr");
+		miGrafo.mostrar();
+		//miGrafo.mostrarOrdenTopologico();
+	}
+	catch (string error) {
+		cerr << error << endl;
+	}
+
+}
